9d4e61d8ab011d65e45976ac68a49b3ca2ea04ab
vanda <vandait996@gmail.com>
2024-12-23 22:53:58 +0700

    create data doctor

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -29,14 +29,18 @@ class OutStandingDoctor extends Component {
   render() {
     let arrDoctors = this.state.arrDoctors;
     let { language } = this.props;
-    arrDoctors = arrDoctors.concat(arrDoctors).concat(arrDoctors);
-    console.log("arrDoctors", arrDoctors);
+    // arrDoctors = arrDoctors.concat(arrDoctors).concat(arrDoctors);
+    // console.log("arrDoctors", arrDoctors);
     return (
       <div className="section-share section-outstanding-doctor">
         <div className="section-container">
           <div className="section-header">
-            <span className="title-section">Bác sĩ nổi bật tuần qua</span>
-            <button className="btn-section">xem thêm</button>
+            <span className="title-section">
+              <FormattedMessage id="homepage.outstanding-doctor" />
+            </span>
+            <button className="btn-section">
+              <FormattedMessage id="homepage.more-infor" />
+            </button>
           </div>
           <div className="section-body">
             <Slider {...this.props.settings}>
